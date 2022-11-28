6af8b7d3accd27860021726fc6aeda8283975d97
shiv10 <shivanshsharma2012@gmail.com>
2022-11-28 20:18:36 +0530

    feat: add prompt box and submit button

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
+import { useRef } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
 
 const Home = () => {
+
+  const userInput = useRef('');
+
   return (
     <div className="root">
       <Head>
@@ -11,11 +15,21 @@ const Home = () => {
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>Ad Copy Generator</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>Add the product type, and a few keywords and see the magic happen</h2>
           </div>
+        </div>
+        <div className="prompt-container">
+          <textarea placeholder="start typing here" className="prompt-box" ref={userInput}/>
+        <div className="prompt-buttons">
+          <a className="generate-button" onClick={null}>
+            <div className="generate">
+              <p>Generate</p>
+            </div>
+          </a>
+        </div>
         </div>
       </div>
       <div className="badge-container grow">
